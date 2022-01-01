1a7af0610c7e08cc049c73ff48557462ee9bd2da
Nitin Kumar <nitin.kumar15@sap.com>
2022-01-01 17:22:53 +0530

    moved driver to test/resources

--- src/test/java/TestGoogle2.java
+++ src/test/java/TestGoogle2.java
@@ -1,17 +1,24 @@
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.time.Duration;
 
+@Slf4j
 public class TestGoogle2 {
 
     static WebDriver driver;
 
     @Test(priority = 1)
     void setup() {
-        System.setProperty("webdriver.chrome.driver", "/Users/I514807/Downloads/chromedriver");
+        String path = "src/test/resources/chromedriver";
+        File file = new File(path);
+        String absolutePath = file.getAbsolutePath();
+        log.info("chrome driver: {}" + absolutePath);
+        System.setProperty("webdriver.chrome.driver", absolutePath);
         driver = new ChromeDriver();
         driver.get("https://google.com");
         driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(2));
