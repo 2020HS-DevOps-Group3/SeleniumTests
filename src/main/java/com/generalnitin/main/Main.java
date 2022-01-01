3f4f26a7596347b2e90c95ab4651dd3478c284cc
Nitin Kumar <nitin.kumar15@sap.com>
2022-01-01 16:31:13 +0530

    first commit

---
+++ src/main/java/com/generalnitin/main/Main.java
@@ -0,0 +1,30 @@
+package com.generalnitin.main;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver", "/Users/I514807/Downloads/chromedriver");
+//
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://google.com");
+//
+//        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(2));
+////        driver.wait(1000);
+//
+//        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"))
+//                .sendKeys("Hello World");
+//        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]")).click();
+//        System.out.println(driver.getTitle());
+//
+//        driver.close();
+//        driver.quit();
+    }
+}
---
+++ src/test/java/TestGoogle.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestGoogle {
+
+    static WebDriver driver;
+
+    @Test(priority = 1)
+    void setup() {
+        System.setProperty("webdriver.chrome.driver", "/Users/I514807/Downloads/chromedriver");
+        driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(2));
+    }
+
+    @Test(priority = 2)
+    void googleTest() {
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"))
+                .sendKeys("Hello World");
+        driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]")).click();
+        System.out.println(driver.getTitle());
+    }
+
+    @Test(priority = 3)
+    void teardown() {
+        driver.close();
+        driver.quit();
+    }
+}
